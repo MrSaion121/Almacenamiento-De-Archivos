d60d81c1841ff314d8ec9cfe11c9432695463b38
Santiago Oseguera <santiagooseguera19@gmail.com>
2024-11-04 18:24:29 -0600

    Pantallas de login y registro

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,9 @@
 const router = require('express').Router();
+const path = require('path')
+const loginRoutes = require('./login')
+const registerRoutes = require('./register')
+
+router.use('/login', loginRoutes)
+router.use('/register', registerRoutes)
 
 module.exports = router; 
