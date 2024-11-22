5f88868282986347d546da698e5beb13775e1a8d
DESK-VM\TellesVM <victor.telles@iteso.mx>
2024-11-21 20:05:18 -0600

    Intento 4 Almacenamiento usuario en RDS

--- src/models/user.js
+++ src/models/user.js
@@ -33,14 +33,18 @@ class UserModel {
     }
 
     async createUser(id_usuario, password) {
+        let conn;
         try {
             const pool = await this.connect();
+            conn = await pool.getConnection();
             const query = `INSERT INTO usuarios (id_usuario, password) VALUES (?, ?)`;
             const [result] = await pool.execute(query, [id_usuario, password]);
             return result;
         } catch (error) {
             console.error('Error creating user:', error);
             throw new Error('Error al crear usuario');
+        } finally {
+            if (conn) conn.release();
         }
     }
 }
